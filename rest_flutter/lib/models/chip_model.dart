9c30ecf1aa6187cea2bd4d199364991e80bc6f90
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-21 09:12:59 -0500

    Crear modelo para el chip
    Crear modelo para el chip

---
+++ rest_flutter/lib/models/chip_model.dart
@@ -0,0 +1,14 @@
+class ChipModel {
+  String colorIcon;
+  String colorBg;
+  String label;
+  String icon;
+
+  ChipModel.constructor(
+      String colorIcon, String colorBg, String label, String icon) {
+    this.colorIcon = colorBg;
+    this.colorBg = colorBg;
+    this.label = label;
+    this.icon = icon;
+  }
+}
