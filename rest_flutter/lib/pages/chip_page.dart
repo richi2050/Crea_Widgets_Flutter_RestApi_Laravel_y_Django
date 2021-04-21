48febfb4ff6f2acb286085ffe91329da111c2d74
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-20 23:12:06 -0500

    Construir el ruteo en el main
    Construir el ruteo en el main

--- rest_flutter/lib/pages/chip_page.dart
+++ rest_flutter/lib/pages/chip_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:rest_flutter/utils/custom_drawer.dart';
 
 class ChipPage extends StatelessWidget {
   static const String ROUTE = '/chips';
   @override
-  Widget build(BuildContext context){
-    return Container(
-      child: Chip(
-        avatar: Icon(Icons.access_alarm),
-        label: Text('Ricardo ')
-      )
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chip Pages'),
+      ),
+      drawer: CustomDrawer.getDrawer(context),
+      body: Chip(avatar: Icon(Icons.access_alarm), label: Text('Ricardo ')),
     );
   }
 }
