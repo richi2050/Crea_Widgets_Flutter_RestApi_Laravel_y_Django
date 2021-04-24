e82b4893ed290deb0431d1671dc8879fc493e1e8
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-23 19:32:24 -0500

    Incluir Bootstrap y el módulo de usuario
    Incluir Bootstrap y el módulo de
    usuario

--- restflutter/routes/web.php
+++ restflutter/routes/web.php
@@ -24,3 +24,7 @@ Route::get('/', function () {
 Route::resource('chip', ChipController::class);
 Route::resource('button', ButtonController::class);
 Route::resource('text', TextController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
